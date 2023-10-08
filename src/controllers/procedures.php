856ee1327510f33d819bcbd68f1f91a575e3843a
Mad Martigane <martigane.mad@gmail.com>
2023-10-08 11:55:45 +0000

    wip

--- src/controllers/procedures.php
+++ src/controllers/procedures.php
@@ -9,8 +9,15 @@ class Headers {
     private $supportedContentTypes = Array(
         'application/json'
     );
-    private $contentType;
+    private string $contentType;
+    private string $http;
+    private $additionnalHeaders = Array();
 
+
+    public function __construct() {
+        $this->setHttp();
+        $this->setContentType();
+    }
 
     public function setContentType(string $value = null) {
         if(in_array($value, $this->supportedContentTypes)) {
@@ -20,51 +27,91 @@ class Headers {
         }
     }
 
+    public function setHttp(string $http = null) {
+        $this->http = $http || '200 OK';
+    }
+
     public function toArray() {
         if (!$this->contentType) {
             $this->setContentType();
         }
 
-        return Array(
-            'Content-Type: ' . $this->contentType
-        );
+        if (!$this->http) {
+            $this->setHttp();
+        }
+
+        return array_merge(Array(
+            'Content-Type: ' . $this->contentType,
+            'HTTP/1.1 ' . $this->http
+        ), $this->additionnalHeaders);
+    }
+
+    public function addCustom(string $header) {
+        array_push($this->additionnalHeaders, $header);
     }
 }
 
 class Procedure {
     // CONSTANTS
-    private $AVAILABLE_PROCEDURE_TYPES;
+    private $AVAILABLE_PROCEDURES;
     private $ctrl;
     private $data;
     private $headers;
     private $type;
 
 
-    public function __construct(string $type, $data) {
+    public function __construct(string $code, $data) {
 
-        $this->AVAILABLE_PROCEDURE_TYPES = Array(
-            (object) Array('name' => 'ok', 'code' => 200, 'default' => true),
-            (object) Array('name' => 'todo', 'code' => 200),
-            (object) Array('name' => 'installation', 'code' => 200)
+        $this->AVAILABLE_PROCEDURES = Array(
+            (object) Array(
+                'code' => 'OK',
+                'type' => 'success',
+                'http' => '200 OK'
+            ),
+            (object) Array(
+                'code' => 'NOT_IMPLEMENTED',
+                'type' => 'error',
+                'http' => '501 Not Implemented',
+                'defaultMessage' => 'Not implemented function.',
+                'isDefault' => true
+            ),
+            (object) Array(
+                'code' => 'NOT_SUPPORTED',
+                'type' => 'error',
+                'http' => '400 Bad request',
+                'defaultMessage' => 'Not supported request.'
+            ),
+            (object) Array(
+                'code' => 'NOT_FOUND',
+                'type' => 'error',
+                'http' => '404 Not Found',
+                'defaultMessage' => 'Resource not found.'
+            ),
+            (object) Array(
+                'code' => 'UNAUTHORIZED',
+                'type' => 'error',
+                'http' => '401 Unauthorized',
+                'defaultMessage' => 'You are not authorized to do this, pleaes loggin first.'
+            )
         );
 
         $this->ctrl = new CommonController();
-        $this->data = $data;
         $this->headers = new Headers();
+        $this->data = $data;
 
-        $this->setType($type);
+        $this->setType($code);
     }
 
-    private function setType(string $type) {
-        foreach ($this->AVAILABLE_PROCEDURE_TYPES as $TYPE) {
-            if ($TYPE->name === $type) {
+    private function setType(string $code = null) {
+        foreach ($this->AVAILABLE_PROCEDURES as $TYPE) {
+            if ($TYPE->code === $code) {
                 $this->type = $TYPE;
                 return;
             }
         }
 
         foreach ($this->AVAILABLE_PROCEDURE_TYPES as $TYPE) {
-            if ($TYPE->default) {
+            if ($TYPE->isDefault) {
                 $this->type = $TYPE;
                 break;
             }
@@ -72,11 +119,24 @@ class Procedure {
     }
 
     public function print() {
+
+        $this->headers->setHttp($this->type->http);
+
         $result = (object) Array(
-            'procedure' => $this->type->name,
-            'data' => $this->data,
-            'error' => Array()
+            'procedure' => $this->type->code, // This code can trigger action on front side.
+            'data' => null,
+            'error' => null
         );
+
+        if (!$this->data->message && $this->type->defaultMessage) {
+            $this->data->message = $this->type->defaultMessage;
+        }
+
+        if ($this->type->type === "error") {
+            $result->error = $this->data;
+        } else {
+            $result->data = $this->data;
+        }
 
         $this->ctrl->sendOutput(json_encode($result), $this->headers->toArray());
     }
@@ -85,18 +145,67 @@ class Procedure {
 class Procedures
 {
 
-    public function todo() {
-        return new Procedure('todo', (object) Array(message => 'This feature is not yet implemented.' ));
+    private $PROCEDURE_MAPPING;
+
+    public function __construct() {
+
+        $this->PROCEDURE_MAPPING = Array(
+            (object) Array('action' => 'list', 'subject' => 'tournament', 'procedure' => 'listTournament')
+        );
     }
 
-    public function ok($data) {
-        return new Procedure('ok', $data);
+    private function findProcedureFromData($data) {
+        $procedureConfig = null;
+
+        foreach ($this->PROCEDURE_MAPPING as $value) {
+            if ($value->action === $data->action && $value->subject === $data->subject) {
+                $procedureConfig = $value;
+                break;
+            }
+        }
+
+        return $procedureConfig;
+    }
+
+    public function getProcedureFromData($requestData) {
+        message('call to getProcedureFromData().');
+        $procedureConfig = $this->findProcedureFromData($requestData);
+
+        if (!$procedureConfig) {
+            message('procedure NOT found.');
+            return $this->error('NOT_FOUND', $requestData);
+        }
+
+        return $this->{$procedureConfig->procedure}($requestData);
+    }
+
+    public function error(string $code, $requestData, string $customMessage = null) {
+        if ($customMessage) {
+            $requestData->message = $customMessage;
+        }
+
+        return new Procedure($code, $requestData,);
+    }
+
+    public function todo() {
+        return new Procedure('TODO', (object) Array(message => 'This feature is not yet implemented.' ));
+    }
+
+    public function ok($requestData) {
+        return new Procedure('OK', $requestData);
     }
 
     public function firstConnection() {
         return $this->todo();
     }
 
+    public function listTournament($requestData) {
+        $data = (object) Array(
+            'message' => 'Fake implementation of listTournament().'
+        );
+
+        return new Procedure('OK', $data);
+    }
 }
 
 ?>
--- src/index.php
+++ src/index.php
@@ -2,24 +2,31 @@
 
 define("PROJECT_ROOT_PATH", __DIR__ . "/");
 
-try {
-    require_once PROJECT_ROOT_PATH . "controllers/common.php";
-    require_once PROJECT_ROOT_PATH . "controllers/procedures.php";
-
-    $common = new CommonController();
-    $data = $common->getUriSegmentsData();
-
-    $procedures = new Procedures();
-    $ok = $procedures->ok($data);
-    $ok->print();
-} catch (Exception $ex) {
+function exeptionHandler(Throwable $ex) {
 
     header_remove('Set-Cookie');
-    header("HTTP/1.1 400 Bad Request", true);
-    // header("HTTP/1.1 500 Internal Server Error", true);
+    header("HTTP/1.1 500 Internal Server Error", true);
 
-    echo($ex->errorMessage());
+    echo($ex->__toString());
 }
 
-?>
+set_exception_handler('exeptionHandler');
 
+function message(string $log) {
+    echo '<p>' . $log . '</p>';
+}
+
+message('requires…');
+require_once PROJECT_ROOT_PATH . "controllers/common.php";
+require_once PROJECT_ROOT_PATH . "controllers/procedures.php";
+
+message('common…');
+$common = new CommonController();
+$data = $common->getUriSegmentsData();
+
+message('procedures…');
+$procedures = new Procedures();
+$result = $procedures->getProcedureFromData($data);
+$result->print();
+
+?>
